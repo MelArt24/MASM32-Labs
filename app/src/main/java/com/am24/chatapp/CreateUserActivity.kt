f769f260499a3541c181cb18afb779ee1016d709
MelArt24 <animator2404@gmail.com>
2025-03-20 19:58:38 +0200

    Created CreateUserActivity.kt and activity_create_user.xml

---
+++ app/src/main/java/com/am24/chatapp/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.am24.chatapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+
+    fun generateColorClicked() {
+
+    }
+}
